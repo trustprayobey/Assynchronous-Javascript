8a5ca0d461f429ff2bcf4fc0fedfb556ca647371
Brian Rono <brishadow24@gmail.com>
2022-08-05 09:07:27 +0300

    doing assynchronous javascript

---
+++ index.js
@@ -0,0 +1,36 @@
+//synchronous , Assynchronous, Callbacks, Promises
+
+// console.log('I eat')
+// console.log('Ice cream')
+// console.lg('with a')
+// console.log('spoon')
+
+//Assynchronous
+
+// console.log('I eat')
+// setTimeout(function () {
+//     console.log('Ice cream')
+// }, 4000)
+
+// console.log('with a')
+// console.log('spoon')
+
+//Callbacks
+
+function receipt(money) {
+    console.log('Oder receipt')
+    money()
+}
+setTimeout(function () {
+    console.log('Production Finished')
+}, 4000)
+setTimeout(function () {
+    console.log('Welcome again')
+}, 10000)
+
+function production() {
+    console.log('Production has started')
+
+}
+
+receipt(production)
