d09d249a75c91af0ed2bf3ef39615babc320e970
Brian Rono <brishadow24@gmail.com>
2022-08-05 09:23:28 +0300

    I made progress

--- index.js
+++ index.js
@@ -17,20 +17,27 @@
 
 //Callbacks
 
-function receipt(money) {
-    console.log('Oder receipt')
-    money()
+
+function combined() {
+    function receipt(money) {
+        console.log('Oder receipt')
+        money()
+    }
+    setTimeout(function () {
+        console.log('Production Finished')
+    }, 4000)
+    setTimeout(function () {
+        console.log('Welcome again')
+    }, 10000)
+
+    function production() {
+        console.log('Production has started')
+
+    }
+
+    receipt(production)
 }
-setTimeout(function () {
-    console.log('Production Finished')
-}, 4000)
-setTimeout(function () {
-    console.log('Welcome again')
-}, 10000)
 
-function production() {
-    console.log('Production has started')
 
-}
-
-receipt(production)
+const button = document.getElementById('btn')
+button.addEventListener('click', combined)
