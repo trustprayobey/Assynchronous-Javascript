b9128d83a14a75c8515c79db984a889dc6e7cf1c
Brian Rono <brishadow24@gmail.com>
2022-08-06 18:51:59 +0300

    async js promise.all

--- index.js
+++ index.js
@@ -81,36 +81,49 @@
 
 //Async js Promises
 
-let posts = [
-    { title: 'Post one', body: 'My post one' },
-    { title: 'Post two', body: 'My post two' }
-]
+// let posts = [
+//     { title: 'Post one', body: 'My post one' },
+//     { title: 'Post two', body: 'My post two' }
+// ]
 
 
-function getPosts() {
-    setTimeout(() => {
-        let output = ''
-        posts.forEach((post) => {
-            output += `<li>${post.title}</li>`
-        })
-        document.body.innerHTML = output
-    }, 1000)
-}
+// function getPosts() {
+//     setTimeout(() => {
+//         let output = ''
+//         posts.forEach((post) => {
+//             output += `<li>${post.title}</li>`
+//         })
+//         document.body.innerHTML = output
+//     }, 1000)
+// }
 
-function createPost(post) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            posts.push(post)
+// function createPost(post) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             posts.push(post)
 
-            const error = false   ///Async js promises
+//             const error = true//Should be false  
+//             ///Async js promises
 
-            if (!error) {
-                resolve()
-            } else {
-                reject('Kuna error Msee')
-            }
-        }, 3000)
-    })
-}
-createPost({ title: 'my new post three', body: 'I used async' })
-    .then(getPosts)
+//             if (!error) {
+//                 resolve()
+//             } else {
+//                 reject('Kuna error Msee')
+//             }
+//         }, 3000)
+//     })
+// }
+
+// createPost({ title: 'my new post three', body: 'I used async' })
+//     .then(getPosts)
+//     .catch(err => console.log(err))
+
+//To avoid doing . dot ///Using the promise ,all
+
+const promise1 = Promise.resolve('Hello world')
+const promise2 = 10
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 2000, 'Goodbye')
+})
+
+Promise.all([promise1, promise2, promise3]).then(values => console.log(values))
